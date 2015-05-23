265f3cf6e2f8fd454cad294cee96fc7833ccf188
David Birch <birch.dave@gmail.com>
2015-05-23 10:13:24 +0000

    raw program validations

--- spec/models/raw_program_spec.rb
+++ spec/models/raw_program_spec.rb
@@ -4,6 +4,46 @@ RSpec.describe RawProgram, type: :model do
   
   it "has a valid factory" do
     expect(FactoryGirl.create(:raw_program)).to be_valid
-  end  
+  end
+  
+  it "is invalid without a title" do
+    expect(FactoryGirl.build(:raw_program, title: nil)).to validate_presence_of(:title)
+  end
+  
+  it "is invalid without a region_name" do
+    expect(FactoryGirl.build(:raw_program, region_name: nil)).to validate_presence_of(:region_name)
+  end
+  
+  it "is invalid without a channel_xmltv_id" do
+    expect(FactoryGirl.build(:raw_program, channel_xmltv_id: nil)).to validate_presence_of(:channel_xmltv_id)
+  end
+    
+  it "is invalid without a start date/time" do
+    expect(FactoryGirl.build(:raw_program, start_datetime: nil)).to validate_presence_of(:start_datetime)
+  end
+  
+  it "is invalid without an end date/time" do
+    expect(FactoryGirl.build(:raw_program, end_datetime: nil)).to validate_presence_of(:end_datetime)
+  end
+    
+  describe "can select all historic raw_programs" do
+      
+    context "where the program is in the future" do
+      it "should not return an array containing the valid raw programs" do
+        valid_raw_program = FactoryGirl.create(:valid_raw_program)
+        historic_raw_programs = RawProgram.historic || []
+        expect(historic_raw_programs).not_to include(valid_raw_program) 
+      end
+    end
+        
+    context "where the program is in the past" do
+      it "is should return an array containing the historic raw program" do
+        historic_raw_program = FactoryGirl.create(:historic_raw_program)
+        historic_raw_programs = RawProgram.historic || []
+        expect(historic_raw_programs).to include(historic_raw_program)
+      end
+    end
+    
+  end 
   
 end
