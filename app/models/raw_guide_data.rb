671ec1ac597155ff415d7587a9e79028fb94d7ec
David Birch <birch.dave@gmail.com>
2015-05-24 11:27:34 +0000

    Added raw guide data object for the conversion

---
+++ app/models/raw_guide_data.rb
@@ -0,0 +1,79 @@
+class RawGuideData
+  @log = Logger.new(File.expand_path("#{Rails.root}/log/raw_guide_data.log", __FILE__))
+       
+  def self.convert #converts the program and channel data
+    begin
+      @log.info("raw_guide_coonversion started...")
+      convert_raw_channels
+      convert_raw_programs
+      create_conversion_summary
+      write_conversion_summary_to_log
+      @log.info("raw_guide_conversion completed.")
+    rescue => e
+      @log.error("#{e.message}")
+      @log.error("#{e.backtrace}")          
+    end
+  end
+  
+  private
+  
+    def self.convert_raw_channels
+      @raw_channel_count = RawChannel.count
+      #@starting_channel_count = Channel.count
+      @starting_channel_count = 0
+      @channels_created = 0
+      @channels_skipped = 0
+      
+      # ** code here **
+      
+      #@final_channel_count = Channel.count
+      @final_channel_count = 0
+      #RawChannel.delete_all
+      @final_raw_channel_count = RawChannel.count
+    end
+    
+    def self.convert_raw_programs
+      @starting_raw_program_count = RawProgram.count 
+      @historic_raw_program_count = RawProgram.historic.count
+      #RawProgram.historic.delete_all
+      
+      @starting_program_count = 0
+      @historic_program_count = 0
+      #@starting_program_count = Program.count      
+      #@historic_program_count = Program.historic.count
+      #Program.historic.delete_all
+      @remaining_raw_program_count = RawProgram.count
+      @remaining_program_count = 0
+      #@remaining_program_count = Program.count
+      @programs_created = 0
+      @programs_skipped = 0
+      
+      # ** code here **
+      
+      @final_program_count = 0
+      #@final_program_count = Program.count
+      #RawProgram.delete_all
+      @final_raw_program_count = RawProgram.count
+    end
+    
+    def self.create_conversion_summary
+      #ConversionSummary.create(
+      #  :name       => raw_channel.channel_name,
+      #  :short_name => raw_channel.channel_name[0,4],
+      #  :xmltv_id   => raw_channel.xmltv_id 
+      #)   
+    end
+    
+    def self.write_conversion_summary_to_log
+      @log.info("+-------------+-----------+-----------+-----------+-----------+-----------+-----------+")
+      @log.info("|             | Starting  | Historic  | Remaining |  Created  |  Skipped  | Remaining |")
+      @log.info("+-------------+-----------+-----------+-----------+-----------+-----------+-----------+")
+      @log.info("|Raw Channels |    #{@raw_channel_count.to_s.rjust(6)} |       N/A |       N/A |       N/A |       N/A |    #{@final_raw_channel_count.to_s.rjust(6)} |")
+      @log.info("|Channels     |    #{@starting_channel_count.to_s.rjust(6)} |       N/A |    #{@starting_channel_count.to_s.rjust(6)} |    #{@channels_created.to_s.rjust(6)} |    #{@channels_skipped.to_s.rjust(6)} |    #{@final_channel_count.to_s.rjust(6)} |")
+      @log.info("|Raw Programs |    #{@starting_raw_program_count.to_s.rjust(6)} |    #{@historic_raw_program_count.to_s.rjust(6)} |    #{@remaining_raw_program_count.to_s.rjust(6)} |       N/A |       N/A |    #{@final_raw_program_count.to_s.rjust(6)} |")
+      @log.info("|Programs     |    #{@starting_program_count.to_s.rjust(6)} |    #{@historic_program_count.to_s.rjust(6)} |    #{@remaining_program_count.to_s.rjust(6)} |    #{@programs_created.to_s.rjust(6)} |    #{@programs_skipped.to_s.rjust(6)} |    #{@final_program_count.to_s.rjust(6)} |")
+      @log.info("+-------------+-----------+-----------+-----------+-----------+-----------+-----------+")  
+    end
+    
+  
+end
