f0c9f0c24bebb4943a60a65a173d3fdf9567824f
David Birch <birch.dave@gmail.com>
2015-05-31 09:09:45 +0000

    change logging

--- app/models/raw_guide_data.rb
+++ app/models/raw_guide_data.rb
@@ -6,7 +6,7 @@ class RawGuideData
       @log.info("raw_guide_coonversion started...")
       convert_raw_channels
       convert_raw_programs
-      create_conversion_summary
+      #create_conversion_summary
       write_conversion_summary_to_log
       @log.info("raw_guide_conversion completed.")
     rescue => e
@@ -42,31 +42,43 @@ class RawGuideData
       @historic_raw_program_count = RawProgram.historic.count
       #RawProgram.historic.delete_all
       
-      @starting_program_count = 0
       @historic_program_count = 0
-      #@starting_program_count = Program.count      
-      #@historic_program_count = Program.historic.count
-      #Program.historic.delete_all
+      @starting_program_count = Program.count      
       @remaining_raw_program_count = RawProgram.count
-      @remaining_program_count = 0
-      #@remaining_program_count = Program.count
+      @remaining_program_count = Program.count
       @programs_created = 0
       @programs_skipped = 0
       
-      # ** code here **
+      RawProgram.find_each do |raw_program|
+        program = Program.create_from_raw_program(raw_program)
+        if program.new_record?
+          @programs_skipped = @programs_skipped + 1
+        else
+          @programs_created = @programs_created + 1
+        end
+      end
       
-      @final_program_count = 0
-      #@final_program_count = Program.count
-      #RawProgram.delete_all
+      @final_program_count = Program.count
+      RawProgram.delete_all
       @final_raw_program_count = RawProgram.count
     end
     
     def self.create_conversion_summary
-      #ConversionSummary.create(
-      #  :name       => raw_channel.channel_name,
-      #  :short_name => raw_channel.channel_name[0,4],
-      #  :xmltv_id   => raw_channel.xmltv_id 
-      #)   
+      ConversionSummary.create(
+        :raw_channel_count       => @raw_channel_count,
+        :final_raw_channel_count => @final_raw_channel_count,
+        :starting_channel_count  => @starting_channel_count,
+        :channels_created => @channels_created,
+        :channels_skipped => @channels_skipped,
+        :final_channel_count => @final_channel_count,
+        :raw_program_count => @raw_program_count,
+        :final_raw_program_count => @final_raw_program_count,
+        :starting_program_count  => @starting_program_count,
+        :programs_created => @programs_created,
+        :programs_skipped => @programs_skipped,
+        :final_program_count => @final_program_count,
+        :conversion_completed => true
+      )   
     end
     
     def self.write_conversion_summary_to_log
