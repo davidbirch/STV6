516248fedaed1f28396f514048c6e43d2c4aec13
David Birch <birch.dave@gmail.com>
2015-07-13 12:26:56 +0000

    Tidy up some deprecated tests

--- spec/routing/raw_channels_routing_spec.rb
+++ spec/routing/raw_channels_routing_spec.rb
@@ -7,28 +7,8 @@ RSpec.describe RawChannelsController, type: :routing do
       expect(:get => "/raw-channels").to route_to("raw_channels#index")
     end
 
-    it "routes to #new" do
-      expect(:get => "/raw-channels/new").to route_to("raw_channels#new")
-    end
-
     it "routes to #show" do
       expect(:get => "/raw-channels/1").to route_to("raw_channels#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/raw-channels/1/edit").to route_to("raw_channels#edit", :id => "1")
-    end
-
-    it "routes to #create" do
-      expect(:post => "/raw-channels").to route_to("raw_channels#create")
-    end
-
-    it "routes to #update" do
-      expect(:put => "/raw-channels/1").to route_to("raw_channels#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/raw-channels/1").to route_to("raw_channels#destroy", :id => "1")
     end
 
   end
--- spec/routing/raw_programs_routing_spec.rb
+++ spec/routing/raw_programs_routing_spec.rb
@@ -7,28 +7,8 @@ RSpec.describe RawProgramsController, type: :routing do
       expect(:get => "/raw-programs").to route_to("raw_programs#index")
     end
 
-    it "routes to #new" do
-      expect(:get => "/raw-programs/new").to route_to("raw_programs#new")
-    end
-
     it "routes to #show" do
       expect(:get => "/raw-programs/1").to route_to("raw_programs#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/raw-programs/1/edit").to route_to("raw_programs#edit", :id => "1")
-    end
-
-    it "routes to #create" do
-      expect(:post => "/raw-programs").to route_to("raw_programs#create")
-    end
-
-    it "routes to #update" do
-      expect(:put => "/raw-programs/1").to route_to("raw_programs#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/raw-programs/1").to route_to("raw_programs#destroy", :id => "1")
     end
 
   end
