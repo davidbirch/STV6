ed9be09d1b454925eea87e3544e20d2d6c7cf762
David Birch <birch.dave@gmail.com>
2015-09-02 11:59:11 +0000

    update to sport select field

--- app/assets/javascripts/guide_onchange.js
+++ app/assets/javascripts/guide_onchange.js
@@ -1,14 +1,14 @@
 function guide_onchange_handler(){
     
         var region_select_field = document.getElementById("region");
-        var region_name = region_select_field.options[region_select_field.selectedIndex].text;
+        var region_name = region_select_field.options[region_select_field.selectedIndex].value;
         var sport_select_field = document.getElementById("sport");
-        var sport_name = sport_select_field.options[sport_select_field.selectedIndex].text;
+        var sport_name = sport_select_field.options[sport_select_field.selectedIndex].value;
         
         //Forward browser to new url
         var new_url = ('/' + region_name)
         if (sport_name != "") {
                 new_url = new_url + '/' + sport_name
         }
-        window.location = new_url.toLowerCase() ;
+        window.location = new_url ;
 }
